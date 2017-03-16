3041b1a628236f093d8b972f6ca92034cee438cd
Lukas Breuer <lukas.breuer@outlook.com>
2017-03-16 23:31:33 +0100

    [google] Evaluate term length
    Closes #61

--- modules/plugins/google.go
+++ modules/plugins/google.go
@@ -4,6 +4,7 @@ import (
     "fmt"
     "github.com/bwmarrin/discordgo"
     "net/url"
+    "git.lukas.moe/sn0w/Karen/helpers"
 )
 
 type Google struct{}
@@ -20,6 +21,11 @@ func (g *Google) Init(session *discordgo.Session) {
 }
 
 func (g *Google) Action(command string, content string, msg *discordgo.Message, session *discordgo.Session) {
+    if len(content) < 2 {
+        session.ChannelMessageSend(msg.ChannelID, helpers.GetText("plugins.google.no-term"))
+        return
+    }
+
     session.ChannelMessageSend(msg.ChannelID, fmt.Sprintf(
         "<https://lmgtfy.com/?q=%s>",
         url.QueryEscape(content),
