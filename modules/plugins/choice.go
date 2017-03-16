38a0e1eb0ce464c8f5e94402721bb56cd1273870
Lukas Breuer <lukas.breuer@outlook.com>
2017-03-16 23:19:04 +0100

    Update choice plugin
    * Now uses GetText() instead of hardcoded strings
    * Refuses to choose when @mentions are present
    * Remove backticks from string
    * Complains about odd inputs
    
    Fixes #65
    Fixes #59

--- modules/plugins/choice.go
+++ modules/plugins/choice.go
@@ -4,6 +4,7 @@ import (
     "github.com/bwmarrin/discordgo"
     "math/rand"
     "strings"
+    "git.lukas.moe/sn0w/Karen/helpers"
 )
 
 type Choice struct{}
@@ -21,11 +22,18 @@ func (c *Choice) Init(session *discordgo.Session) {
 
 func (c *Choice) Action(command string, content string, msg *discordgo.Message, session *discordgo.Session) {
     if !strings.Contains(content, "|") {
-        session.ChannelMessageSend(msg.ChannelID, ":x: You need to pass multiple options separeated by `|`")
+        session.ChannelMessageSend(msg.ChannelID, helpers.GetText("plugins.choice.wrong-syntax"))
+        return
+    }
+
+    if len(msg.Mentions) > 0 {
+        session.ChannelMessageSend(msg.ChannelID, helpers.GetText("plugins.choice.no-mentions"))
         return
     }
 
     choices := strings.Split(content, "|")
-
-    session.ChannelMessageSend(msg.ChannelID, "I've chosen `"+choices[rand.Intn(len(choices))]+"` :smiley:")
+    session.ChannelMessageSend(msg.ChannelID, helpers.GetTextF(
+        "plugins.choice.result",
+        strings.TrimSpace(choices[rand.Intn(len(choices))])),
+    )
 }
