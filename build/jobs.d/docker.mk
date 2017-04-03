080b47a406fbfd1487bb337bb009881b327bc1a6
Lukas Breuer <lukas.breuer@outlook.com>
2017-04-03 22:10:29 +0200

    Update dockerfiles

--- build/jobs.d/docker.mk
+++ build/jobs.d/docker.mk
@@ -1,2 +1,7 @@
-container:
-	docker build --squash -t sn0w/karen -f build/Dockerfile .
+container-build:
+	docker build --squash -t sn0w/karen-build -f build/build.dockerfile .
+
+container-runtime:
+	docker build --squash -t sn0w/karen-runtime -f build/runtime.dockerfile .
+
+containers: container-build container-runtime
