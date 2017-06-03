621d67bcf5d16007465ec5f485bb0750691210b9
Folling <j.purol@web.de>
2017-06-03 12:21:55 +0200

    Enlarge function fixed

--- src/modules/plugins/enlarge.go
+++ src/modules/plugins/enlarge.go
@@ -4,6 +4,7 @@ import (
     "github.com/bwmarrin/discordgo"
     "regexp"
     "code.lukas.moe/x/karen/src/helpers"
+    "strings"
 )
 
 type Enlarge struct{}
@@ -11,6 +12,7 @@ type Enlarge struct{}
 func (p *Enlarge) Commands() []string {
     return []string{
         "enlarge",
+        "wumbo",
     }
 }
 
@@ -19,14 +21,15 @@ func (p *Enlarge) Init(session *discordgo.Session) {
 }
 
 func (p *Enlarge) Action(command string, content string, msg *discordgo.Message, session *discordgo.Session) {
-    emojis := regexp.MustCompile(`.*?<:\w+:\d+>.*?`).FindAllString(msg.Content, -1)
+    regex := regexp.MustCompile(`.*?<:\w+:\d+>.*?`)
+    emojis := regex.FindAllString(msg.Content, -1)
     emojiID := emojis[0]
-    foundEmoji, _ := regexp.MatchString(`.*?<:\w+:\d+>.*?`, msg.Content)
-    if !foundEmoji {
-        _, err := session.ChannelMessageSend(msg.ChannelID, "I wasn't able to find an emoji :sad:")
+    emojiInformation := strings.Split(emojiID, ":")
+    if len(emojis) == 0 {
+        _, err := session.ChannelMessageSend(msg.ChannelID, "I wasn't able to find an emoji :frowning:")
         helpers.Relax(err)
         return
     }
-    _,err := session.ChannelMessageSend(msg.ChannelID, discordgo.EndpointEmoji(emojiID))
+    _,err := session.ChannelMessageSend(msg.ChannelID, "https://cdn.discordapp.com/emojis/" + emojiInformation[1] + "png")
     helpers.Relax(err)
 }
