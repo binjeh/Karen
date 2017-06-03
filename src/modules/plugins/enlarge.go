377501a1a34c3f5003065a1c91d7c9370af23b48
Lukas Breuer <lukas.breuer@outlook.com>
2017-06-03 18:45:03 +0200

    Apply recommended changes for [p]enlarge
    This commit applied the patch that I submitted to folling.
    XREF: https://gist.github.com/sn0w/06466b4144c94a2c0adc1d406bb41286
    
    @Folling <3

--- src/modules/plugins/enlarge.go
+++ src/modules/plugins/enlarge.go
@@ -4,7 +4,6 @@ import (
     "github.com/bwmarrin/discordgo"
     "regexp"
     "code.lukas.moe/x/karen/src/helpers"
-    "strings"
     "net/url"
 )
 
@@ -22,22 +21,24 @@ func (p *Enlarge) Init(session *discordgo.Session) {
 }
 
 func (p *Enlarge) Action(command string, content string, msg *discordgo.Message, session *discordgo.Session) {
-    regex := regexp.MustCompile(`.*?<:(\w+):(\d+)>.*?`)
-    emojis := regex.FindStringSubmatch(msg.Content)
-    emojiID := emojis[0]
-    emojiInformation := strings.Split(emojiID, ":")
+    emojis := regexp.MustCompile(`.*?<:(\w+):(\d+)>.*?`).FindStringSubmatch(content)
+
     if len(emojis) == 0 {
         _, err := session.ChannelMessageSend(msg.ChannelID, "I wasn't able to find an emoji :frowning:")
         helpers.Relax(err)
         return
     }
-    _, urlErr := url.ParseRequestURI("https://cdn.discordapp.com/emojis/" + emojiInformation[2] + ".png")
 
+    emojiID := emojis[2]
+    uri := "https://cdn.discordapp.com/emojis/" + emojiID + ".png"
+
+    _, urlErr := url.ParseRequestURI(uri)
     if urlErr != nil {
         _, err := session.ChannelMessageSend(msg.ChannelID, "Error resolving the URL")
         helpers.Relax(err)
         return
     }
-    _,err := session.ChannelMessageSend(msg.ChannelID, "https://cdn.discordapp.com/emojis/" + emojiInformation[2] + ".png")
+
+    _, err := session.ChannelMessageSend(msg.ChannelID, uri)
     helpers.Relax(err)
 }
