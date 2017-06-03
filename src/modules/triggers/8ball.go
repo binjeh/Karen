42bdf3879160a6cdc9dbbb418db6ee4a7064b583
Lukas Breuer <lukas.breuer@outlook.com>
2017-06-03 19:15:14 +0200

    Revert "Let's do this licensing thingy for real :thinking:"
    This reverts commit 4e0e88e733be56fafe7c7b0489b17799a7217ad5.

---
+++ src/modules/triggers/8ball.go
@@ -0,0 +1,20 @@
+package triggers
+
+import "code.lukas.moe/x/karen/src/helpers"
+
+type EightBall struct{}
+
+func (e *EightBall) Triggers() []string {
+    return []string{
+        "8ball",
+        "8",
+    }
+}
+
+func (e *EightBall) Response(trigger string, content string) string {
+    if len(content) < 3 {
+        return helpers.GetText("triggers.8ball.ask_a_question")
+    }
+
+    return ":8ball: " + helpers.GetText("triggers.8ball")
+}
